978d53b87013e630baf973388625f2efb922b430
Nicolas vauché <hello@nicolasvauche.net>
2024-04-08 12:46:44 +0200

    Using Sequelize to connect a MariaDB Server

--- Projet Fil Rouge - Correction/server.js
+++ Projet Fil Rouge - Correction/server.js
@@ -1,23 +1,33 @@
 require('dotenv').config()
 const app = require('./app')
+const { connectDB } = require('./services/db')
 
 let port = process.env.APP_PORT || 3000
 
 const startServer = port => {
-  const server = app
-    .listen(port, () => {
-      console.log(`Server is listening on http://localhost:${port}`)
+  connectDB()
+    .then(() => {
+      const server = app
+        .listen(port, () => {
+          console.log(`Server is listening on http://localhost:${port}`)
+        })
+        .on('error', err => {
+          if (err.code === 'EADDRINUSE') {
+            console.error(
+              `Port ${port} is already in use, trying port ${
+                parseInt(port) + 1
+              }…`
+            )
+            startServer(parseInt(port) + 1)
+          } else {
+            console.error(err)
+            process.exit(1)
+          }
+        })
     })
-    .on('error', err => {
-      if (err.code === 'EADDRINUSE') {
-        console.error(
-          `Port ${port} is already in use, trying port ${parseInt(port) + 1}…`
-        )
-        startServer(parseInt(port) + 1)
-      } else {
-        console.error(err)
-        process.exit(1)
-      }
+    .catch(err => {
+      console.error('Database connection failed:', err)
+      process.exit(1)
     })
 }
 
